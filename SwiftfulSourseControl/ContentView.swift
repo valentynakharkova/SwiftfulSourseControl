7e7ace88d92b70d518546870fba5ab3720461905
Valentyna Kharkova <gerry29072001@gmail.com>
2025-08-04 15:28:28 +0300

    My first commit

--- SwiftfulSourseControl/ContentView.swift
+++ SwiftfulSourseControl/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "heart.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.indigo)
+            Text("Valentyna Kharkova")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .padding()
+                .background(.indigo)
+                .cornerRadius(10)
+
         }
         .padding()
     }
