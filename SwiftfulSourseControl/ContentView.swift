60c6689f7d03384eb7a2d7f938274d3423fa8f98
Valentyna Kharkova <gerry29072001@gmail.com>
2025-08-04 14:43:54 +0300

    Initial Commit

---
+++ SwiftfulSourseControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulSourseControl
+//
+//  Created by Valentyna Kharkova on 04.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulSourseControl/SwiftfulSourseControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourseControlApp.swift
+//  SwiftfulSourseControl
+//
+//  Created by Valentyna Kharkova on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourseControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
