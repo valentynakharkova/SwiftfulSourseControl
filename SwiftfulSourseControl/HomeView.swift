120b64fae525246c509fb55ae290277b51fd72bb
Valentyna Kharkova <gerry29072001@gmail.com>
2025-08-04 15:43:03 +0300

    Add HomeView

---
+++ SwiftfulSourseControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourseControl
+//
+//  Created by Valentyna Kharkova on 04.08.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Second View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
