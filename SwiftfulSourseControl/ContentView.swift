c1b87b9475901d6c61cfcf49127a07535a8e6cc9
Valentyna Kharkova <gerry29072001@gmail.com>
2025-08-13 15:18:30 +0300

    [Feature] Change to Globe

--- SwiftfulSourseControl/ContentView.swift
+++ SwiftfulSourseControl/ContentView.swift
@@ -41,7 +41,7 @@ struct ContentView: View {
     var body: some View {
         ScrollView {
             VStack {
-                Image(systemName: "magnifyingglass")
+                Image(systemName: "globe")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100, height: 100)
