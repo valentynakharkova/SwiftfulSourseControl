3d2ce6677cbe9a0a64c9560f6830dc36b7836e3e
Valentyna Kharkova <gerry29072001@gmail.com>
2025-08-13 15:18:01 +0300

    [Feature] Change title

--- SwiftfulSourseControl/ContentView.swift
+++ SwiftfulSourseControl/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
                     .scaledToFit()
                     .frame(width: 100, height: 100)
                     .foregroundStyle(.indigo)
-                Text("Swiftful Thinking!")
+                Text("Val")
                     .font(.largeTitle)
                     .foregroundStyle(.white)
                     .padding()
