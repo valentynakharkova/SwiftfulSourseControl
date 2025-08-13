d652d6304e1587471b68155551e56214d4ffc692
Valentyna Kharkova <gerry29072001@gmail.com>
2025-08-13 15:19:16 +0300

    [Feature] Change button title

--- SwiftfulSourseControl/ContentView.swift
+++ SwiftfulSourseControl/ContentView.swift
@@ -52,7 +52,7 @@ struct ContentView: View {
                     .padding()
                     .background(.indigo)
                     .cornerRadius(10)
-                Button("Subscribe") {
+                Button("Subscribe now") {
                     
                 }
                 .foregroundStyle(.white)
