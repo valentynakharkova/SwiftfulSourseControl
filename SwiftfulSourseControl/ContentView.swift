ab86ffa8ede9af87fc1c1c820b60c12c6a85dd30
Valentyna Kharkova <gerry29072001@gmail.com>
2025-08-14 16:14:34 +0300

    Crappy commit massege

--- SwiftfulSourseControl/ContentView.swift
+++ SwiftfulSourseControl/ContentView.swift
@@ -56,7 +56,7 @@ struct ContentView: View {
                     .padding()
                     .background(.indigo)
                     .cornerRadius(10)
-                Button("Subscribe now") {
+                Button("Subscribe") {
                     
                 }
                 .foregroundStyle(.white)
